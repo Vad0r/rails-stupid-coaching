d6f7e552ca81f6e03455d80a3b6100e5e4050075
kevin decker <kdecker.webmaster@gmail.com>
2021-08-09 15:16:54 +0200

    finish

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,20 @@
+class QuestionsController < ApplicationController
+
+  def ask
+
+  end
+
+  def answer
+    @answer = coach_answer(params[:question])
+  end
+
+  def coach_answer(question)
+    if question == 'I am going to work right now!'
+      ''
+    elsif question.end_with?('?')
+      'Silly question, get dressed and go to work!'
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+end
